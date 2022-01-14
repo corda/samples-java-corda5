af992f4fefb1eefe11a472266a3037811db319e1
peterli-r3 <peter.li@r3.com>
2022-01-14 19:40:21 +0800

    change boardingTicket variable

--- Tutorial/missionmars/contracts/src/main/java/net/corda/missionMars/states/BoardingTicket.java
+++ Tutorial/missionmars/contracts/src/main/java/net/corda/missionMars/states/BoardingTicket.java
@@ -9,6 +9,7 @@ import net.corda.v5.ledger.contracts.ContractState;
 import net.corda.v5.serialization.annotations.ConstructorForDeserialization;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 @BelongsToContract(BoardingTicketContract.class)
@@ -18,33 +19,33 @@ public class BoardingTicket implements ContractState, JsonRepresentable {
     private String description;
     private Party marsExpress;
     private Party owner;
-    private int daysUntilLaunch;
+    private LocalDate launchDate;
 
     //Constructors
     @ConstructorForDeserialization
-    public BoardingTicket(String description, Party marsExpress, Party owner, int daysUntilLaunch) {
+    public BoardingTicket(String description, Party marsExpress, Party owner, LocalDate launchDate) {
         this.description = description;
         this.marsExpress = marsExpress;
         this.owner = owner;
-        this.daysUntilLaunch = daysUntilLaunch;
+        this.launchDate = launchDate;
     }
 
-    public BoardingTicket(String description, Party marsExpress, int daysUntilLaunch) {
+    public BoardingTicket(String description, Party marsExpress, LocalDate launchDate) {
         this.description = description;
         this.marsExpress = marsExpress;
         this.owner = marsExpress;
-        this.daysUntilLaunch = daysUntilLaunch;
+        this.launchDate = launchDate;
     }
 
     //Getters
     public String getDescription() {return description;}
     public Party getMarsExpress() {return marsExpress;}
     public Party getOwner() {return owner;}
-    public int getdaysUntilLaunch() {return daysUntilLaunch;}
+    public LocalDate getlaunchDate() {return launchDate;}
 
     //helper method
     public BoardingTicket changeOwner(Party owner){
-        BoardingTicket newOwnerState = new BoardingTicket(this.description,this.marsExpress,owner,this.daysUntilLaunch);
+        BoardingTicket newOwnerState = new BoardingTicket(this.description,this.marsExpress,owner,this.launchDate);
         return newOwnerState;
     }
 
@@ -54,7 +55,7 @@ public class BoardingTicket implements ContractState, JsonRepresentable {
         return "description : " + this.description +
                 " marsExpress : " + this.marsExpress.getName().toString() +
                 " owner : " + this.owner.getName().toString() +
-                " daysUntilLaunch : " + Integer.toString(this.daysUntilLaunch);
+                " launchDate : " + this.launchDate.toString();
     }
 
     @NotNull
